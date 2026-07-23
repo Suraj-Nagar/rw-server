8dbf33b1de525c5dc61065dc1e0df150590a42f6
Dhakad <surajnagar5755@gmail.com>
2026-07-23 08:43:21 +0530

    add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Razorpay from 'razorpay';
-import app from '../server/app.js';
+import app from './app.js';
 import connectionTodb from "./config/db.js";
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5001;
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:5173','http://192.168.0.116:5173'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://192.168.0.116:5173'],
     credentials: true
   }
 });
